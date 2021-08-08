cb69f30f568aa258a9974b74177836cc5856ec81
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 22:55:48 +0300

    Update bubbleSort.js

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,12 +1,13 @@
 async function bubbleSort(graph,textElement,numbers,colors){
-
-    let curr,i=0;
+    let curr,i=0,switched;
     for(i=0;i<numbers;i++){
         curr=0;
+        switched=false;
         for(j=1;j<numbers-i;j++){
             counter++;
             graph[curr].style.fill=graph[j].style.fill=colors[4];
             if(Number(graph[curr].getAttribute("height"))>Number(graph[j].getAttribute("height"))){
+                switched=true;
                 await delay(time);
                 switchNodesPosition(graph[curr],graph[j],textElement[curr],textElement[j]);
                 [graph[curr],graph[j]]=[graph[j],graph[curr]];
@@ -20,5 +21,9 @@ async function bubbleSort(graph,textElement,numbers,colors){
             curr=j;
         }
         graph[j-1].style.fill=colors[1];
+        if(switched==false)
+            for(i=0;i<numbers;i++){
+                graph[i].style.fill=colors[1];
+            }
     }
 }
