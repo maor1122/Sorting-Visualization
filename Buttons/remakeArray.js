ae3f58714d36c6d1a301d3e7ead825554cf68d7b
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 15:05:29 +0300

    Update remakeArray.js

--- Buttons/remakeArray.js
+++ Buttons/remakeArray.js
@@ -1,4 +1,4 @@
-function remakeArray(){
+export function remakeArray(){
     numbers=arraySizeSlider.value;
     window.location.href=window.location.href.split('?')[0]+"?numbers="+numbers+"&speed="+time;
 }
