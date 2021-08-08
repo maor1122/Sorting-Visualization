5461ab316d96139da5ab19c96d12ac349c05971b
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 14:41:15 +0300

    Create Buttons.js

---
+++ Buttons.js
@@ -0,0 +1,6 @@
+import bubbleSortStarter from "./buttons/bubbleSortStarter.js";
+import selectionSortStarter from "./buttons/selectionSort.js";
+import mergeSortStarter from "./buttons/mergeSortStarter.js";
+import quickSortStarter from "./buttons/quickSortStarter.js";
+import selectionSortStarter from "./buttons/selectionSortStarter.js";
+import remakeArray from "./buttons/remakeArray.js";
