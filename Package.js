b5c07ab7dfc24550f4baf5c6847c776a3f47aac7
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 14:47:02 +0300

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,13 @@
+//buttons.
+import bubbleSortStarter from "./buttons/bubbleSortStarter.js";
+import selectionSortStarter from "./buttons/selectionSort.js";
+import mergeSortStarter from "./buttons/mergeSortStarter.js";
+import quickSortStarter from "./buttons/quickSortStarter.js";
+import selectionSortStarter from "./buttons/selectionSortStarter.js";
+import remakeArray from "./buttons/remakeArray.js";
+//algorithms.
+import bubbleSort from "./Algorithms/bubbleSort.js";
+import insertionSort from "./Algorithms/insertionSort.js";
+import mergeSort from "./Algorithms/mergeSort.js";
+import quickSort from "./Algorithms/quickSort.js";
+import selectionSort from "./Algorithms/selectionSort.js";
