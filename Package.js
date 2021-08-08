27cb362abe8fbf5daef003cbf590536b02b85dd3
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 14:50:25 +0300

    Update Package.js

--- Package.js
+++ Package.js
@@ -1,6 +1,5 @@
 //buttons.
 import bubbleSortStarter from "./buttons/bubbleSortStarter.js";
-import selectionSortStarter from "./buttons/selectionSort.js";
 import mergeSortStarter from "./buttons/mergeSortStarter.js";
 import quickSortStarter from "./buttons/quickSortStarter.js";
 import selectionSortStarter from "./buttons/selectionSortStarter.js";
