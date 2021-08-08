41fb9c82247b674fd303659fb24ac4d3f6f7b7ff
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 15:44:47 +0300

    Update quickSortStarter.js

--- buttons/quickSortStarter.js
+++ buttons/quickSortStarter.js
@@ -1,4 +1,4 @@
-export async function quickSortStarter(){
+async function quickSortStarter(){
     if(!sorting&&!sorted){
         sorting=true;
         await quickSort(0,numbers-1,graph,textElement,colors);
