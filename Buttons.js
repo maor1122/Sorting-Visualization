cd0f2db1aa5ef7099b787262b2e44243681c1423
Maor Shemesh <62097305+maor1122@users.noreply.github.com>
2021-08-08 13:17:35 +0300

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -1,3 +1,5 @@
+//This file contains the functions that triggered with the buttons.
+
 function remakeArray(){
     numbers=arraySizeSlider.value;
     window.location.href=window.location.href.split('?')[0]+"?numbers="+numbers+"&speed="+time;
